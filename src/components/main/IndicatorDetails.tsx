043dd789e0a3fccfb9891efc4da0023402c0f380
ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-12-15 16:17:39 +0200

    refactor(IndicatorDetails): remove unused CardTitle import

--- src/components/main/IndicatorDetails.tsx
+++ src/components/main/IndicatorDetails.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import useGetFilterData from "@/hooks/useGetFilterData";
 import { Gauge, Link2, Calculator, ChartNoAxesCombined } from "lucide-react";
 
